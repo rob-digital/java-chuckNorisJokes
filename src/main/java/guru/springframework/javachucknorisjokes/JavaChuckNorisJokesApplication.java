82ffbe7123b990e89a6cf3357c1322d10ff2ced5
rokselar <robert.roksela@gmail.com>
2022-08-01 16:49:07 +0100

    spring app created

---
+++ src/main/java/guru/springframework/javachucknorisjokes/JavaChuckNorisJokesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.javachucknorisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChuckNorisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChuckNorisJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/javachucknorisjokes/JavaChuckNorisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.javachucknorisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChuckNorisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
